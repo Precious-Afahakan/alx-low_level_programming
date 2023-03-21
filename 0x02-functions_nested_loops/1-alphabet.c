33c2738eb17e8124397406c1f810c6f581a63b38
Precious-Afahakan <afahakanprecious@gmail.com>
2023-03-21 11:12:46 -0700

    assignment 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /*
- * main -print all alphabets in lowercase followed by a new line
+ * print_alphabets -print all alphabets in lowercase followed by a new line
 */
 void print_alphabet(void);
 {
